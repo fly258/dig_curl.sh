e4e674823c6f7bae141c829bafcd44715db324a8
fly258 <39674500+fly258@users.noreply.github.com>
2019-08-30 15:34:06 +0800

    Update dig_curl.sh
    增加了备注信息

--- dig_curl.sh
+++ dig_curl.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
-# Create Date: 2018-09-29
+# Create Date: 2019-09-3o
 # Author:      fly
 # Mail:       fly@258.com
-# Version:     1.0
-# Explain:   write for nping test
+# Version:     2.0
+# Explain:   write for dig url
 bash=$(cd "$(dirname "$0")"; pwd)
 cd `echo ${bash}`
 source /etc/profile
 
 
-dns=211.140.13.188
-cur_time=`date +%Y%m%d%H%M`
+dns=211.140.13.188  ##配置解析的DNS
+cur_time=`date +%Y%m%d%H%M` ##日记输出时间
 
-while read URL
+while read URL  
 
 do
 	echo "$URL" >> dig_result_$cur_time.log
-	dig @$dns +short "$URL" > aa
+	dig @$dns +short "$URL" > aa  ##dig输出内容可以根据自己情况修改
 	cat aa |grep -v ^[a-z] >> dig_result_$cur_time.log
 	echo "" >> dig_result_$cur_time.log
-done < url
+done < url ##需要解析的域名每行一条在脚本目录里面的url文件里
 
 sleep  5
 
-awk '{if($0 !~ /^$/)printf "%s ",$0;else print}' dig_result_$cur_time.log >> dig_end_"$cur_time".txt
+awk '{if($0 !~ /^$/)printf "%s ",$0;else print}' dig_result_$cur_time.log >> dig_end_"$cur_time".txt ## log输出结果
